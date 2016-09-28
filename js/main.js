3107c23d15d67ff2199c3d6ceae074d841a7e023
rensramos <rensramos17@gmail.com>
2016-09-28 11:26:21 +0800

    Add type js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@
 var main = main || {};
 main.init = function() {
     main.onNavbarMenuClick();
-    main.iconAnimation();
+    // main.iconAnimation();
+    main.onHeaderChange();
+};
+
+main.onHeaderChange = function () {
+    $(".intro-header-middle").typed({
+        strings: ["FRONT END DEVELOPER ^1000", "WEBSITE DESIGNER", "FRONT END DEVELOPER"],
+        typeSpeed: 10,
+        backDelay: 3000,
+        showCursor: true,
+        cursorChar: "|",
+        loop: false
+    });
 };
 
 main.iconAnimation = function () {
@@ -13,44 +25,44 @@ main.iconAnimation = function () {
 
 main.onNavbarMenuClick = function () {
 
-    var triggerBttn = document.getElementById( 'trigger-overlay' ),
-        overlay = document.querySelector( 'div.overlay' ),
-        closeBttn = overlay.querySelector( 'button.overlay-close' );
-    transEndEventNames = {
-        'WebkitTransition': 'webkitTransitionEnd',
-        'MozTransition': 'transitionend',
-        'OTransition': 'oTransitionEnd',
-        'msTransition': 'MSTransitionEnd',
-        'transition': 'transitionend'
-    },
-        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-        support = { transitions : Modernizr.csstransitions };
-
-    function toggleOverlay() {
-        if( classie.has( overlay, 'open' ) ) {
-            classie.remove( overlay, 'open' );
-            classie.add( overlay, 'close' );
-            var onEndTransitionFn = function( ev ) {
-                if( support.transitions ) {
-                    if( ev.propertyName !== 'visibility' ) return;
-                    this.removeEventListener( transEndEventName, onEndTransitionFn );
-                }
-                classie.remove( overlay, 'close' );
-            };
-            if( support.transitions ) {
-                overlay.addEventListener( transEndEventName, onEndTransitionFn );
-            }
-            else {
-                onEndTransitionFn();
-            }
-        }
-        else if( !classie.has( overlay, 'close' ) ) {
-            classie.add( overlay, 'open' );
-        }
-    }
-
-    triggerBttn.addEventListener( 'click', toggleOverlay );
-    closeBttn.addEventListener( 'click', toggleOverlay );
+    // var triggerBttn = document.getElementById( 'trigger-overlay' ),
+    //     overlay = document.querySelector( 'div.overlay' ),
+    //     closeBttn = overlay.querySelector( 'trigger-overlay' );
+    // transEndEventNames = {
+    //     'WebkitTransition': 'webkitTransitionEnd',
+    //     'MozTransition': 'transitionend',
+    //     'OTransition': 'oTransitionEnd',
+    //     'msTransition': 'MSTransitionEnd',
+    //     'transition': 'transitionend'
+    // },
+    //     transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+    //     support = { transitions : Modernizr.csstransitions };
+    //
+    // function toggleOverlay() {
+    //     if( classie.has( overlay, 'open' ) ) {
+    //         classie.remove( overlay, 'open' );
+    //         classie.add( overlay, 'close' );
+    //         var onEndTransitionFn = function( ev ) {
+    //             if( support.transitions ) {
+    //                 if( ev.propertyName !== 'visibility' ) return;
+    //                 this.removeEventListener( transEndEventName, onEndTransitionFn );
+    //             }
+    //             classie.remove( overlay, 'close' );
+    //         };
+    //         if( support.transitions ) {
+    //             overlay.addEventListener( transEndEventName, onEndTransitionFn );
+    //         }
+    //         else {
+    //             onEndTransitionFn();
+    //         }
+    //     }
+    //     else if( !classie.has( overlay, 'close' ) ) {
+    //         classie.add( overlay, 'open' );
+    //     }
+    // }
+    //
+    // triggerBttn.addEventListener( 'click', toggleOverlay );
+    // closeBttn.addEventListener( 'click', toggleOverlay );
 
 };
 
