399b7a6a4b74cb7c64032abd9ee7538f3d98934f
rensramos <rensramos17@gmail.com>
2016-11-16 14:04:09 +0800

    Update function structure

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 
-var main = main || {};
-main.init = function() {
-    main.onNavbarMenuClick();
-    // main.iconAnimation();
-    main.onHeaderChange();
-    main.mixItUpItem();
-    // main.testimonialCarousel();
-};
 
-main.mixItUpItem = function () {
+$(function () {
     var $container = $('#project-container');
     $container.mixItUp({
         controls: {
@@ -34,9 +26,10 @@ main.mixItUpItem = function () {
             }
         }
     });
-};
+});
 
-main.onHeaderChange = function () {
+//Intro header type animation
+$(function () {
     $(".intro-header-middle").typed({
         strings: ["FRONT END DEVELOPER ^1000", "WEBSITE DESIGNER", "FRONT END DEVELOPER"],
         typeSpeed: 10,
@@ -45,59 +38,37 @@ main.onHeaderChange = function () {
         cursorChar: "|",
         loop: false
     });
-};
-
-main.iconAnimation = function () {
-    $('#icon-transition').on('click', function () {
-        $(this).toggleClass('open');
-    });
-};
-
-main.onNavbarMenuClick = function () {
-
-    // var triggerBttn = document.getElementById( 'trigger-overlay' ),
-    //     overlay = document.querySelector( 'div.overlay' ),
-    //     closeBttn = overlay.querySelector( 'trigger-overlay' );
-    // transEndEventNames = {
-    //     'WebkitTransition': 'webkitTransitionEnd',
-    //     'MozTransition': 'transitionend',
-    //     'OTransition': 'oTransitionEnd',
-    //     'msTransition': 'MSTransitionEnd',
-    //     'transition': 'transitionend'
-    // },
-    //     transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    //     support = { transitions : Modernizr.csstransitions };
-    //
-    // function toggleOverlay() {
-    //     if( classie.has( overlay, 'open' ) ) {
-    //         classie.remove( overlay, 'open' );
-    //         classie.add( overlay, 'close' );
-    //         var onEndTransitionFn = function( ev ) {
-    //             if( support.transitions ) {
-    //                 if( ev.propertyName !== 'visibility' ) return;
-    //                 this.removeEventListener( transEndEventName, onEndTransitionFn );
-    //             }
-    //             classie.remove( overlay, 'close' );
-    //         };
-    //         if( support.transitions ) {
-    //             overlay.addEventListener( transEndEventName, onEndTransitionFn );
-    //         }
-    //         else {
-    //             onEndTransitionFn();
-    //         }
-    //     }
-    //     else if( !classie.has( overlay, 'close' ) ) {
-    //         classie.add( overlay, 'open' );
-    //     }
-    // }
-    //
-    // triggerBttn.addEventListener( 'click', toggleOverlay );
-    // closeBttn.addEventListener( 'click', toggleOverlay );
-
-};
-
-
-$(function(){
-    main.init();
 });
 
+//testimonial
+$(function () {
+    $("#testimonial-carousel").carousel({
+        interval: 15000
+    });
+});
+
+//category filter dropdown animation
+$(function () {
+    $('.dropdown').on('show.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
+
+    $('.dropdown').on('hide.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+    });
+});
+
+//on form sumbit
+$("#form-content").on("submit",function(){
+    $("#ss-submit").attr("disabled", true);
+    setTimeout(function(){
+        $("#ss-submit").attr("disabled", false);
+    }, 1000);
+    $("#hidden_iframe").on('load',function () {
+        $("#modal-message").modal('show');
+        $("#form-content")[0].reset();
+        setTimeout(function(){
+            $("#modal-message").modal('hide');
+        }, 2000);
+    });
+});
